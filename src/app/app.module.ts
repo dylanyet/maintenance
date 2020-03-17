f67bd9929db60dba9e586a9dea40e7b91f320714
DylanYet <dylanyet99@gmail.com>
2020-03-17 21:26:19 +0800

    maintenance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaintenanceComponent } from './maintenance/maintenance.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MaintenanceComponent,
-    LoginComponent
+
   ],
   imports: [
     BrowserModule,
